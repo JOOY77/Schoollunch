63da91d3070c21e49a1420d14cb594946c6a8d2e
JOOY77 <rhkrehdgns07@gmail.net>
2025-04-26 19:38:47 +0900

    아이콘 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { MealProvider } from "@/lib/contexts/MealContext"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "학교 급식 웹",
+  title: "울산경의고등학교 급식 웹",
   description: "우리 학교 급식 정보 웹",
   viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
 }
@@ -19,6 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className={inter.className}>
         <MealProvider>{children}</MealProvider>
       </body>
